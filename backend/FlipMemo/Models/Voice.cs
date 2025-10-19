8e99bd7d2f54b4706c0ea5556036b9617d9ad162
Martin Saraga <ms55962@fer.hr>
2025-10-19 15:52:31 +0200

    Rijesen #15, #19, #20

---
+++ backend/FlipMemo/Models/Voice.cs
@@ -0,0 +1,14 @@
+namespace FlipMemo.Models
+{
+    public class Voice
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int WordId { get; set; }
+        public int Score { get; set; }
+        public DateTime LastReviewed { get; set; }
+        public User User { get; set; } = null!;
+        public Word Word { get; set; } = null!;
+
+    }
+}
