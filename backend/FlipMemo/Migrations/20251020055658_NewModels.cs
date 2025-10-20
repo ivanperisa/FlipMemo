d774442f284c012f394c52a9df4f77795415c507
Ivan Periša <ivan.perisa@abysalto.eu>
2025-10-20 08:07:09 +0200

    Add new migration and change FlipMemo.sln file path

---
+++ backend/FlipMemo/Migrations/20251020055658_NewModels.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FlipMemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dictionaries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Language = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dictionaries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    WordId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ForeignWord = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    ForeignPhrase = table.Column<string>(type: "text", nullable: false),
+                    CroatianWord = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CroatianPhrases = table.Column<string>(type: "text", nullable: false),
+                    AudioFile = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.WordId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DictionaryWords",
+                columns: table => new
+                {
+                    DictionaryId = table.Column<int>(type: "integer", nullable: false),
+                    WordId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DictionaryWords", x => new { x.DictionaryId, x.WordId });
+                    table.ForeignKey(
+                        name: "FK_DictionaryWords_Dictionaries_DictionaryId",
+                        column: x => x.DictionaryId,
+                        principalTable: "Dictionaries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DictionaryWords_Words_WordId",
+                        column: x => x.WordId,
+                        principalTable: "Words",
+                        principalColumn: "WordId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserWords",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    WordId = table.Column<int>(type: "integer", nullable: false),
+                    Box = table.Column<int>(type: "integer", nullable: false),
+                    LastReviewed = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    NextReview = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Learned = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserWords", x => new { x.UserId, x.WordId });
+                    table.ForeignKey(
+                        name: "FK_UserWords_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserWords_Words_WordId",
+                        column: x => x.WordId,
+                        principalTable: "Words",
+                        principalColumn: "WordId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Voices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    WordId = table.Column<int>(type: "integer", nullable: false),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    LastReviewed = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Voices_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Voices_Words_WordId",
+                        column: x => x.WordId,
+                        principalTable: "Words",
+                        principalColumn: "WordId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DictionaryWords_WordId",
+                table: "DictionaryWords",
+                column: "WordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserWords_WordId",
+                table: "UserWords",
+                column: "WordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voices_UserId",
+                table: "Voices",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Voices_WordId",
+                table: "Voices",
+                column: "WordId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DictionaryWords");
+
+            migrationBuilder.DropTable(
+                name: "UserWords");
+
+            migrationBuilder.DropTable(
+                name: "Voices");
+
+            migrationBuilder.DropTable(
+                name: "Dictionaries");
+
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
