46a729c733be85ff9f0134677fe0859d208b3181
Karlo Jularić <karlo.jularic@fer.hr>
2026-01-13 10:59:00 +0100

    Added xUnit test cases for DictionaryService

--- backend/FlipMemo.Tests/DictionaryServicesTest.cs
+++ backend/FlipMemo.Tests/DictionaryServicesTest.cs
@@ -1,5 +1,8 @@
 ﻿using FlipMemo.Data;
+using FlipMemo.DTOs.WordAndDictionary;
+using FlipMemo.Models;
 using FlipMemo.Services;
+using FlipMemo.Utils;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,10 +12,11 @@ using System.Threading.Tasks;
 
 namespace FlipMemo.Tests
 {
-    internal class DictionaryServicesTest
+    public class DictionaryServicesTest
     {
         private readonly ApplicationDbContext _context;
         private readonly DictionaryService _dictionaryService;
+
         public DictionaryServicesTest()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -21,6 +25,64 @@ namespace FlipMemo.Tests
             _context = new ApplicationDbContext(options);
 
             _dictionaryService = new DictionaryService(_context);
+        }
+
+        [Fact]
+        public async Task CreateDictionaryAsync_DictionaryDoesNotExist_CreatesDictionary()
+        {
+
+            var dto = new CreateDictionaryRequestDto
+            {
+                Name = "Test Dictionary",
+                Language = "English"
+            };
+
+            await _dictionaryService.CreateDictionaryAsync(dto);
+
+            var createdDictionary = await _context.Dictionaries.SingleAsync();
+
+            Assert.Equal("Test Dictionary", createdDictionary.Name);
+            Assert.Equal("English", createdDictionary.Language);
+
+        }
+
+        [Fact]
+        public async Task CreateDictionaryAsync_DictionaryAlreadyExists_ThrowsConflictException()
+        {
+
+            _context.Dictionaries.Add(new Dictionary
+            {
+                Name = "Existing Dictionary",
+                Language = "English"
+            });
+
+            await _context.SaveChangesAsync();
+
+            var dto = new CreateDictionaryRequestDto
+            {
+                Name = "Existing Dictionary",
+                Language = "English"
+            };
+
+            var exception = await Assert.ThrowsAsync<ConflictException>(() => _dictionaryService.CreateDictionaryAsync(dto));
+            Assert.Equal("Dictionary already exists.", exception.Message);
+        }
+
+        [Fact]
+        public async Task GetWordsFromDictionaryAsync_DictionaryDoesNotHaveAnyWords_ThrowsNotFoundException()
+        {
+
+            _context.Dictionaries.Add(new Dictionary
+            {
+                Name = "Empty Dictionary",
+                Language = "English"
+            });
+
+            await _context.SaveChangesAsync();
+            var dictionary = await _context.Dictionaries.SingleAsync();
+
+            var exception = await Assert.ThrowsAsync<NotFoundException>(() => _dictionaryService.GetWordsFromDictionaryAsync(dictionary.Id));
+            Assert.Equal("Dictionary doesn't have any words.", exception.Message);
         }
     }
 }
