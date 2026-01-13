0e9d1818ac1d618c12c6e5dce8cd721ebd3fe9e9
Karlo Jularić <karlo.jularic@fer.hr>
2026-01-13 10:12:50 +0100

    Initial setup of DictionaryServiceTest

---
+++ backend/FlipMemo.Tests/DictionaryServicesTest.cs
@@ -0,0 +1,26 @@
+﻿using FlipMemo.Data;
+using FlipMemo.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipMemo.Tests
+{
+    internal class DictionaryServicesTest
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DictionaryService _dictionaryService;
+        public DictionaryServicesTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _context = new ApplicationDbContext(options);
+
+            _dictionaryService = new DictionaryService(_context);
+        }
+    }
+}
