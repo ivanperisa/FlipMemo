58dda2e1f2aee5fb090fbd45c09a10da2160fefa
Ivan Periša <ivan.perisa@fer.hr>
2026-01-15 23:10:57 +0100

    program.cs fix

--- backend/FlipMemo/Program.cs
+++ backend/FlipMemo/Program.cs
@@ -124,21 +124,21 @@ builder.Services.AddAuthorizationBuilder()
 builder.Services.AddHttpClient<IWordsApiService, WordsApiService>(client =>
 {
     client.BaseAddress = new Uri("https://wordsapiv1.p.rapidapi.com/");
-    client.DefaultRequestHeaders.Add("X-RapidAPI-Key", builder.Configuration["RapidApi:SendGridApiKey"]);
+    client.DefaultRequestHeaders.Add("X-RapidAPI-Key", builder.Configuration["RapidApi:ApiKey"]);
     client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "wordsapiv1.p.rapidapi.com");
 });
 
 builder.Services.AddHttpClient<IWordDictionaryApiService, WordDictionaryApiService>(client =>
 {
     client.BaseAddress = new Uri("https://twinword-word-graph-dictionary.p.rapidapi.com/");
-    client.DefaultRequestHeaders.Add("X-RapidAPI-Key", builder.Configuration["RapidApi:SendGridApiKey"]);
+    client.DefaultRequestHeaders.Add("X-RapidAPI-Key", builder.Configuration["RapidApi:ApiKey"]);
     client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "twinword-word-graph-dictionary.p.rapidapi.com");
 });
 
 builder.Services.AddHttpClient<IDeepTranslateApiService, DeepTranslateApiService>(client =>
 {
     client.BaseAddress = new Uri("https://deep-translate1.p.rapidapi.com/");
-    client.DefaultRequestHeaders.Add("X-RapidAPI-Key", builder.Configuration["RapidApi:SendGridApiKey"]);
+    client.DefaultRequestHeaders.Add("X-RapidAPI-Key", builder.Configuration["RapidApi:ApiKey"]);
     client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "deep-translate1.p.rapidapi.com");
 });
 
